f56a30bc96199d6dff8ff13a4272305bb09bba3e
zys1106 <zys17666541106@163.com>
2019-08-04 21:37:28 +0800

    login页面封装完成

---
+++ App/Datas/user_info.py
@@ -0,0 +1,10 @@
+"""
+-*-enconding: utf-8
+@Time:2019-08-04 20:45
+@Author:grassroadsZ
+@File:user_info.py
+Motto：good good study , day day up !!!
+"""
+
+right_info = {"phone": '18684720553',
+              "pwd": "python"}
